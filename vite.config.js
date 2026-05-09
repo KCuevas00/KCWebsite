3d0655919b9593a0d991e01d273c5bd81248fad5
Z1928756 <z1928756@students.niu.edu>
2026-05-09 15:48:37 -0500

    github pages should now be deployable

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: './',
   root: '.',
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
